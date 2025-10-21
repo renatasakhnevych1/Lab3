6e5feab5c96b1e3962dd90cbe9c61feddca9735d
Renata <nway2419@gmail.com>
2025-10-21 16:43:05 +0200

    commit

--- Lab3/Lab3.cpp
+++ Lab3/Lab3.cpp
@@ -1,20 +1,185 @@
-﻿// Lab3.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
 
-#include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_CTYPE, "polish");
+
+    //ZAD 1
+    /*int a, b;
+    cout << "Podaj pierwszą liczbę"<<endl;
+    cin >> a;
+    cout << "Podaj drugą liczbę"<<endl;
+    cin >> b;
+    if (a == b) {
+        cout << "Liczby są równe"<<endl;
+    }
+    else if (a < b) {
+        cout << "Druga liczba jest większa" << endl;
+    }
+    else {
+        cout << "Pierwsza liczba jest większa" << endl;
+    }*/
+
+
+    //ZAD 2
+    /*int a, b;
+    cout << "Podaj pierwszą liczbę" << endl;
+    cin >> a;
+    cout << "Podaj drugą liczbę" << endl;
+    cin >> b;
+    if (a == b) {
+        cout << "Liczby są równe" << endl;
+        if (a < b) {
+            cout << "Druga liczba jest większa" << endl;
+        }
+    }
+    else {
+        cout << "Pierwsza liczba jest większa" << endl;
+    }*/
+
+    //ZAD 3
+    /*double r;
+    cout << "Podaj r:" << endl;
+    cin >> r;
+    if (0.7 <= r and r <= 1) {
+        cout << "bardzo silna korelacja dodatnia" << endl;
+    }
+    else if (r >= 0.5) {
+        cout << "silna korelacja dodatnia" << endl;
+    }
+    else if (r >= 0.3) {
+        cout << "umiarkowana korelacja dodatnia" << endl;
+    }
+    else if (r >= 0.2) {
+        cout << "słaba korelacja dodatnia" << endl;
+    }
+    else if (r >= 0) {
+        cout << "brak korelacji" << endl;
+    }*/
+
+
+    //ZAD 4
+    /*float a, b;
+    char opcja;
+    float wynik;
+
+    cout << "Podaj pierwszą liczbę: " << endl;
+    cin >> a;
+    cout << "Podaj drugą liczbę: " << endl;
+    cin >> b;
+    cout << "Wybierz działanie (’+’ lub ’-’ lub ’*’ lub ’/’):" << endl;
+    cin >> opcja;
+    switch (opcja) {
+    case '+':
+        wynik = a + b;
+        cout << a << " + " << b<<" = "<< wynik;
+        break;
+    case '-':
+        wynik = a - b;
+        cout << a << " - " << b << " = " << wynik;
+        break;
+    case '*':
+        wynik = a * b;
+        cout << a << " * " << b << " = " << wynik;
+        break;
+    case '/':
+        if (b==0) {
+            cout << "Nie można dzielić przez 0!" << endl;
+            break;
+        }
+        else {
+            wynik = a / b;
+            cout << a << " / " << b << " = " << wynik;
+            break;
+        }
+    default:
+        cout << "Podano inna opcje niż + , -, *, lub /" << endl;
+    }
+    wynik = 0;*/
+
+
+    //ZAD 5
+
+    /*while (1) {
+        cout << "Hello";
+    }*/
+
+
+    //ZAD 6
+
+
+   /* float a, b;
+    char opcja;
+    float wynik;
+    char opcja2 = 't';
+
+    while (opcja2 == 't') {
+        cout << "Podaj pierwszą liczbę: " << endl;
+        cin >> a;
+        cout << "Podaj drugą liczbę: " << endl;
+        cin >> b;
+        cout << "Wybierz działanie (’+’ lub ’-’ lub ’*’ lub ’/’):" << endl;
+        cin >> opcja;
+        switch (opcja) {
+        case '+':
+            wynik = a + b;
+            cout << a << " + " << b << " = " << wynik;
+            break;
+        case '-':
+            wynik = a - b;
+            cout << a << " - " << b << " = " << wynik;
+            break;
+        case '*':
+            wynik = a * b;
+            cout << a << " * " << b << " = " << wynik;
+            break;
+        case '/':
+            if (b == 0) {
+                cout << "Nie można dzielić przez 0!" << endl;
+                break;
+            }
+            else {
+                wynik = a / b;
+                cout << a << " / " << b << " = " << wynik;
+                break;
+            }
+        default:
+            cout << "Podano inna opcje niż + , -, *, lub /" << endl;
+        }
+        wynik = 0;
+        cout << "\n Czy chcesz kontynuować ('t','n') ?" << endl;
+        cin >> opcja2;
+    }*/
+
+
+
+
+        //ZAD 7
+    /*while (1) {
+        for (int i = 0; i <= 200; i++) {
+            if (i % 2 != 0) {
+                cout << i << ", ";
+            }
+        }
+        break;
+    }*/
+
+    
+//ZAD 8
+//int n;
+//cout << "Podaj naturalną liczbę dodatnią :" << endl;
+//cin >> n;
+//for (int i = 2; i <= sqrt(n); i++) {
+//    if (n % i == 0) {
+//        cout << "Pierwszy dzilnik dla n to: " << i << endl;
+//    }
+//    else {
+//        cout << "Nie ma dzielnika z tego przedziału dla tej liczby "<< endl;
+//    }
+//}
+
+
 }
-
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
